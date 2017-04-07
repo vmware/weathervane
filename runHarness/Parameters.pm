6965004a183b080ecebef6d9f2b5a31a746b1f2b
Hal Rosenberg <hrosenbe@vmware.com>
2017-04-07 07:50:47 -0400

    Increment version number for release
    Signed-off-by: Hal Rosenberg <hrosenbe@vmware.com>

--- runHarness/Parameters.pm
+++ runHarness/Parameters.pm
@@ -652,7 +652,7 @@ sub getSingletonInstanceParamHashRef {
 
 }
 
-our $version = "1.0.9";
+our $version = "1.0.10";
 
 # These variables contain the key names for the parameter hashes
 tie( our %parameters, 'Tie::IxHash' );
@@ -1357,7 +1357,7 @@ $parameters{"stopStatsScript"} = {
 
 $parameters{"dockerWeathervaneVersion"} = {
 	"type"      => "=s",
-	"default"   => "1.0.9",
+	"default"   => "1.0.10",
 	"parent"    => "host",
 	"usageText" => "",
 	"showUsage" => 0,

bdab574ccb849688eb87d1cc7f7d6662bb4731be
Hal Rosenberg <hrosenbe@vmware.com>
2017-04-14 15:11:28 -0400

    Add files for Docker images

---
+++ dockerImages/mongodb/entrypoint.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+sigterm()
+{
+   echo "signal TERM received. cmd = $cmd, $numArgs = $numArgs"
+   rm -f /fifo
+   if [ $numArgs -gt 0 ]; then
+  	 eval "$cmd --shutdown"
+   else
+	/usr/bin/mongod -f /etc/mongod.conf --shutdown
+   fi
+   exit 0
+}
+
+sigusr1()
+{
+   echo "signal USR1 received. cmd = $cmd, $numArgs = $numArgs. Reloading"
+   if [ $numArgs -gt 0 ]; then
+   	  eval "$cmd --shutdown"
+	  eval "$cmd &"
+   else
+	/usr/bin/mongod -f /etc/mongod.conf --shutdown
+    /usr/bin/mongod -f /etc/mongod.conf &
+   fi
+}
+
+trap 'sigterm' TERM
+trap 'sigusr1' USR1
+
+echo "search weathervane" >> /etc/resolv.conf 
+
+echo never > /sys/kernel/mm/transparent_hugepage/enabled
+echo never > /sys/kernel/mm/transparent_hugepage/defrag
+
+cmd=$*
+numArgs=$#
+
+if [ $numArgs -gt 0 ]; then
+	eval "$cmd &"
+else
+	/usr/bin/mongod -f /etc/mongod.conf --shutdown
+	/usr/bin/mongod -f /etc/mongod.conf &
+fi
+
+if [ ! -e "/fifo" ]; then
+	mkfifo /fifo || exit
+fi
+chmod 400 /fifo
+
+# wait indefinitely
+while true;
+do
+  echo "Waiting for child to exit."
+  read < /fifo
+  echo "Child Exited"
+done

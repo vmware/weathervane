0586c35f6c614578ce10f2231a7e00c51a0f5b6a
Hal Rosenberg <hrosenbe@vmware.com>
2017-06-26 09:50:14 -0400

    Revert "Users only attend fixed set of auctions rather than choosing at random"
    This reverts commit 3a115ab1c27db00144f9fc00e207b5b4fbaff396.
    
    This functionality needs better testing and will be added back at a later point

--- workloadDriver/src/main/java/com/vmware/weathervane/workloadDriver/benchmarks/auction/operations/GetActiveAuctionsOperation.java
+++ workloadDriver/src/main/java/com/vmware/weathervane/workloadDriver/benchmarks/auction/operations/GetActiveAuctionsOperation.java
@@ -29,16 +29,12 @@ import com.vmware.weathervane.workloadDriver.benchmarks.auction.common.AuctionSt
 import com.vmware.weathervane.workloadDriver.benchmarks.auction.common.AuctionStateManagerStructs.ActiveAuctionProvider;
 import com.vmware.weathervane.workloadDriver.benchmarks.auction.common.AuctionStateManagerStructs.AttendedAuctionsProvider;
 import com.vmware.weathervane.workloadDriver.benchmarks.auction.common.AuctionStateManagerStructs.ContainsActiveAuctions;
-import com.vmware.weathervane.workloadDriver.benchmarks.auction.common.AuctionStateManagerStructs.ContainsFirstAuctionId;
-import com.vmware.weathervane.workloadDriver.benchmarks.auction.common.AuctionStateManagerStructs.FirstAuctionIdListener;
 import com.vmware.weathervane.workloadDriver.benchmarks.auction.common.AuctionStateManagerStructs.LoginResponseProvider;
 import com.vmware.weathervane.workloadDriver.benchmarks.auction.common.AuctionStateManagerStructs.NeedsActiveAuctions;
 import com.vmware.weathervane.workloadDriver.benchmarks.auction.common.AuctionStateManagerStructs.NeedsAttendedAuctions;
 import com.vmware.weathervane.workloadDriver.benchmarks.auction.common.AuctionStateManagerStructs.NeedsLoginResponse;
 import com.vmware.weathervane.workloadDriver.benchmarks.auction.common.AuctionStateManagerStructs.NeedsPageSize;
-import com.vmware.weathervane.workloadDriver.benchmarks.auction.common.AuctionStateManagerStructs.NeedsUsersPerAuction;
 import com.vmware.weathervane.workloadDriver.benchmarks.auction.common.AuctionStateManagerStructs.PageSizeProvider;
-import com.vmware.weathervane.workloadDriver.benchmarks.auction.common.AuctionStateManagerStructs.UsersPerAuctionProvider;
 import com.vmware.weathervane.workloadDriver.benchmarks.auction.representation.AuctionRepresentation;
 import com.vmware.weathervane.workloadDriver.common.core.Behavior;
 import com.vmware.weathervane.workloadDriver.common.core.SimpleUri;
@@ -48,14 +44,12 @@ import com.vmware.weathervane.workloadDriver.common.model.target.Target;
 import com.vmware.weathervane.workloadDriver.common.statistics.StatsCollector;
 
 public class GetActiveAuctionsOperation extends AuctionOperation implements
-		NeedsLoginResponse, NeedsPageSize, NeedsActiveAuctions, ContainsActiveAuctions, 
-		NeedsAttendedAuctions, ContainsFirstAuctionId {
+		NeedsLoginResponse, NeedsPageSize, NeedsActiveAuctions, ContainsActiveAuctions, NeedsAttendedAuctions {
 
 	private LoginResponseProvider _loginResponseProvider;
 	private PageSizeProvider _pageSizeProvider;
 	private ActiveAuctionProvider _liveAuctionProvider;
 	private ActiveAuctionListener _liveAuctionListener;	
-	private FirstAuctionIdListener _firstAuctionIdListener;	
 	private AttendedAuctionsProvider _attendedAuctionsProvider;
 
 	private String _authToken;
@@ -63,7 +57,7 @@ public class GetActiveAuctionsOperation extends AuctionOperation implements
 	private Map<String, String> _authTokenHeaders = new HashMap<String, String>();
 	private Map<String, String> _bindVarsMap = new HashMap<String, String>();
 
-	private boolean _gotPageForUser = false;
+	private boolean _gotNewPage = false;
 	private long _pageFetched = 0;
 	
 	private static final Logger logger = LoggerFactory.getLogger(GetActiveAuctionsOperation.class);
@@ -86,7 +80,7 @@ public class GetActiveAuctionsOperation extends AuctionOperation implements
 			_pageSize = _pageSizeProvider.getItem("pageSize");
 			_authTokenHeaders.put("API_TOKEN", _authToken);	
 			_bindVarsMap.put("pageSize", Long.toString(_pageSize));
-			_gotPageForUser = false;
+			_gotNewPage = false;
 			_pageFetched = 0;
 		}
 				
@@ -111,15 +105,6 @@ public class GetActiveAuctionsOperation extends AuctionOperation implements
 			finalStep();
 			this.setOperationComplete(true);
 			return;
-		} 
-		
-		if (_liveAuctionProvider.getCurrentActiveAuctionsPage() == 0) {
-			/*
-			 * Save the auctionId of the first auction for use in other operations.
-			 * It is used when forming the ID of auctions to join in JoinAuction
-			 */
-			AuctionRepresentation firstAuction = _liveAuctionProvider.getActiveAuctions().get(0);
-			_firstAuctionIdListener.setFirstAuctionId(firstAuction.getId());
 		}
 		
 		/*
@@ -127,7 +112,7 @@ public class GetActiveAuctionsOperation extends AuctionOperation implements
 		 * The initial fetch doesn't count as a new page because it
 		 * isn't random across all possible pages.
 		 */
-		if (_gotPageForUser) {
+		if (_gotNewPage) {
 			/*
 			 * Got a new page.  Need to make sure that the user isn't already attending
 			 * all auctions on the page.
@@ -168,7 +153,7 @@ public class GetActiveAuctionsOperation extends AuctionOperation implements
 			 * Get a new page of active auctions
 			 */
 			getActiveAuctionsStep();
-			_gotPageForUser = true;
+			_gotNewPage = true;
 		}
 		
 	}
@@ -200,7 +185,7 @@ public class GetActiveAuctionsOperation extends AuctionOperation implements
 
 	protected void finalStep() throws Throwable {
 		logger.debug("GetActiveAuctionsOperation:finalStep behaviorID = " + this.getBehaviorId());
-		_gotPageForUser = false;
+		_gotNewPage = false;
 	}
 
 	@Override
@@ -233,9 +218,5 @@ public class GetActiveAuctionsOperation extends AuctionOperation implements
 		return null;
 	}
 	
-	@Override
-	public void registerFirstAuctionIdListener(FirstAuctionIdListener listener) {
-		_firstAuctionIdListener = listener;
-	}
 
 }

ad27fdc5e009fbd2c24302b372c25c0f201b5466
Hal Rosenberg <hrosenbe@vmware.com>
2017-07-18 08:17:01 -0400

    Add gitignore on tomcat Docker image to get all files

---
+++ dockerImages/tomcat/apache-tomcat-auction1/bin/setenv.sh
@@ -0,0 +1,11 @@
+# Edit this file to set custom options
+# Tomcat accepts two parameters JAVA_OPTS and CATALINA_OPTS
+# JAVA_OPTS are used during START/STOP/RUN
+# CATALINA_OPTS are used during START/RUN
+
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CATALINA_HOME/lib
+export LD_LIBRARY_PATH
+
+CATALINA_PID=$CATALINA_BASE/logs/tomcat.pid
+
+CATALINA_OPTS="-Xmx2G -Xms2G -XX:+AlwaysPreTouch  -Dspring.profiles.active=postgresql,imagesInMongo,singleMongo,clusteredRabbit  -DRABBITMQ_HOSTS=AuctionMsg1:5672,AuctionMsg2:5672  -DMONGODB_HOST=AuctionNosql1 -DMONGODB_PORT=27017  -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -Xloggc:/opt/apache-tomcat-auction1/logs/gc.log -DnodeNumber=1"

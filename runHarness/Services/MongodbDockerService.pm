11eb52ac09705cb621f1bd6766a1cefdffc96686
Hal Rosenberg <hrosenbe@vmware.com>
2017-08-10 11:20:15 -0400

    Change configuration of MongoDB container to use env variables

--- runHarness/Services/MongodbDockerService.pm
+++ runHarness/Services/MongodbDockerService.pm
@@ -616,7 +616,7 @@ sub startShardedMongodb {
 
 					# Also use the internal port if the appServer is also using docker and is on
 					# the same (non-host) network as the mongos, but use the docker name rather than the hostname
-					my $mongosDocker = $appServer->host->dockerGetIp($other->getDockerName();
+					my $mongosDocker = $appServer->host->dockerGetIp($dockerName);
 					$appServer->setMongosDocker($mongosDocker);
 				}
 				else {
@@ -676,7 +676,7 @@ sub startShardedMongodb {
 				# the same (non-host) network as the mongos, but use the docker name rather than the hostname
 				$logger->debug("app server and mongos are both dockerized on the same host and network, setting app server to use internal name and port");
 				$appServer->internalPortMap->{'mongos'} = $mongosPort;
-				my $mongosDocker = $appServer->host->dockerGetIp($other->getDockerName();
+				my $mongosDocker = $appServer->host->dockerGetIp($dockerName);
 				$appServer->setMongosDocker($mongosDocker);
 				$hostsMongosCreated{$appIpAddr} = $mongosPort;
 

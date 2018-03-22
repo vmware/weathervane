e3760b0ad3475a1e1692f462a29cfb9fea1415af
Hal Rosenberg <hrosenbe@vmware.com>
2018-03-22 09:23:41 -0400

    Have traffic go directly to nginx service on all nodes

--- runHarness/AppInstance/AuctionKubernetesAppInstance.pm
+++ runHarness/AppInstance/AuctionKubernetesAppInstance.pm
@@ -150,17 +150,24 @@ override 'cleanup' => sub {
 override 'getWwwIpAddrsRef' => sub {
 	my ($self) = @_;
 	my $cluster = $self->host;
+	my $logger = get_logger("Weathervane::AppInstance::AuctionKubernetesAppInstance");
 	
 	my $wwwIpAddrsRef = [];
 	
-	# Get the IP address of the nginx-ingress in this appInstance's namespace
-	my $ipAddr = $cluster->kubernetesGetIngressIp("type=appInstance", $self->namespace);
+	# Get the IP addresses of the nginx-ingress in this appInstance's namespace
+	my $ipAddrsRef = $cluster->kubernetesGetNodeIPs();
+	if ($#{$ipAddrsRef} < 0) {
+		$logger->error("There are no IP addresses for the Kubernetes nodes");
+		exit 1;
+	}
 	
 	# Get the nodePort numbers for the ingress-controller-nginx service
-	my $httpPort = $cluster->kubernetesGetNodePortForPortNumber("app=ingress-controller-nginx", 80, $self->namespace);
-	my $httpsPort = $cluster->kubernetesGetNodePortForPortNumber("app=ingress-controller-nginx", 443, $self->namespace);
+	my $httpPort = $cluster->kubernetesGetNodePortForPortNumber("app=auction,type=webServer", 80, $self->namespace);
+	my $httpsPort = $cluster->kubernetesGetNodePortForPortNumber("app=auction,type=webServer", 443, $self->namespace);
 	
-	push @$wwwIpAddrsRef, [$ipAddr, $httpPort, $httpsPort];					
+	foreach my $ipAddr (@$ipAddrsRef) {
+		push @$wwwIpAddrsRef, [$ipAddr, $httpPort, $httpsPort];							
+	}
 	return $wwwIpAddrsRef;
 };
 

160edb4e98467284a4785caf0141df7c9f6b8a61
Hal Rosenberg <hrosenbe@vmware.com>
2018-06-19 11:27:17 -0400

    Add service and k8s yaml for auctionBidService

---
+++ dockerImages/auctionbidservice/entrypoint.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+sigterm()
+{
+   echo "signal TERM received."
+   rm -f /fifo
+   /opt/apache-tomcat/bin/shutdown.sh -force
+   cat /opt/apache-tomcat-auction1/logs/*
+   rm -f /opt/apache-tomcat-auction1/logs/*
+   exit 0
+}
+
+sigusr1()
+{
+    echo "signal USR1 received. start stats collection"
+    cp /opt/apache-tomcat-auction1/logs/gc.log /opt/apache-tomcat-auction1/logs/gc_rampup.log
+}
+
+trap 'sigterm' TERM
+trap 'sigusr1' USR1
+
+perl /updateResolveConf.pl
+
+rm -f /opt/apache-tomcat-auction1/logs/* 
+perl /configure.pl
+
+if [ $# -gt 0 ]; then
+	eval "$* &"
+else
+    /opt/apache-tomcat/bin/startup.sh  &
+fi
+
+if [ ! -e "/fifo" ]; then
+	mkfifo /fifo || exit
+fi
+chmod 400 /fifo
+
+sleep 30;
+tail -f /opt/apache-tomcat-auction1/logs/gc.log &
+tail -f /opt/apache-tomcat-auction1/logs/auction.log &
+
+# wait indefinitely
+while true;
+do
+  echo "Waiting for child to exit."
+  read < /fifo
+  echo "Child Exited"
+done

bb69aebdd01e602e9d2c35ef96b4d38177578c72
Hal Rosenberg <hrosenbe@vmware.com>
2018-07-31 17:11:31 -0400

    Update version number

--- runHarness/Parameters.pm
+++ runHarness/Parameters.pm
@@ -652,7 +652,7 @@ sub getSingletonInstanceParamHashRef {
 
 }
 
-our $version = "1.0.12";
+our $version = "VMmark3.1";
 
 # These variables contain the key names for the parameter hashes
 tie( our %parameters, 'Tie::IxHash' );
@@ -1357,7 +1357,7 @@ $parameters{"stopStatsScript"} = {
 
 $parameters{"dockerWeathervaneVersion"} = {
 	"type"      => "=s",
-	"default"   => "1.0.12",
+	"default"   => "VMmark3.1",
 	"parent"    => "host",
 	"usageText" => "",
 	"showUsage" => 0,

604f1967abf2f14a209c48d1a61deed21613883e
Hal Rosenberg <hrosenbe@vmware.com>
2019-03-05 15:37:40 -0500

    Initial version of updated dbLoader

---
+++ newDbLoader/HELP.md
@@ -0,0 +1,8 @@
+# Getting Started
+
+### Guides
+The following guides illustrates how to use certain features concretely:
+
+* [Building a RESTful Web Service with Spring Boot Actuator](https://spring.io/guides/gs/actuator-service/)
+* [Accessing Data with JPA](https://spring.io/guides/gs/accessing-data-jpa/)
+
---
+++ newDbLoader/src/main/java/com/vmware/weathervane/auction/dbloader/DbLoaderApplication.java
@@ -0,0 +1,47 @@
+package com.vmware.weathervane.auction.dbloader;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.vmware.weathervane.auction.data.dao.AuctionMgmtDao;
+import com.vmware.weathervane.auction.data.dao.FixedTimeOffsetDao;
+
+@SpringBootApplication
+public class DbLoaderApplication {
+	private static String numThreadsDefault = "30";
+	private static String itemFileDefault = "items.json";
+	private static String creditLimitDefault = "1000000";
+	private static String maxDurationDefault = "0";
+	private static String maxUsersDefault = "120";
+	private static String imageDirDefault = "images";
+
+	private static final double itemDuration = 8.5 * 60;
+	
+	private static List<Thread> threadList = new ArrayList<Thread>();
+
+	private static DbLoaderDao dbLoaderDao;
+	private static AuctionMgmtDao auctionMgmtDao;
+
+	private static FixedTimeOffsetDao fixedTimeOffsetDao;
+
+	private static final Logger logger = LoggerFactory.getLogger(DbLoaderApplication.class);
+
+	public static void usage() {
+
+		System.out.println("Usage information for the Auction DBLoader:");
+
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbLoaderApplication.class, args);
+	}
+
+	public void run(String[] args) {
+		
+	}
+}

b014ebd46980d52f3fbf8454335e7b4f4ec662b7
Hal Rosenberg <hrosenbe@vmware.com>
2019-03-14 09:57:35 -0400

    Switch auctiondatamanager container to use cassandra in scripts

--- dockerImages/auctiondatamanager/loadData.pl
+++ dockerImages/auctiondatamanager/loadData.pl
@@ -7,17 +7,13 @@ use POSIX;
 my $maxUsers = $ENV{'MAXUSERS'};
 my $users = $ENV{'USERS'};
 my $maxDuration = $ENV{'MAXDURATION'};
-my $numNosqlShards = $ENV{'NUMNOSQLSHARDS'};
-my $numNosqlReplicas = $ENV{'NUMNOSQLREPLICAS'};
-my $mongoHostPortPairsString = $ENV{'MONGODBSERVERS'};
 my $workloadNum = $ENV{'WORKLOADNUM'};
 my $appInstanceNum = $ENV{'APPINSTANCENUM'};
 my $springProfilesActive = $ENV{'SPRINGPROFILESACTIVE'};
 my $dbHostname = $ENV{'DBHOSTNAME'};
 my $dbPort = $ENV{'DBPORT'};
-my $mongodbHostname = $ENV{'MONGODBHOSTNAME'};
-my $mongodbPort = $ENV{'MONGODBPORT'};
-my $mongodbReplicaSet = $ENV{'MONGODBREPLICASET'};
+my $cassandraContactpoints = $ENV{'CASSANDRA_CONTACTPOINTS'};
+my $cassandraPort = $ENV{'CASSANDRA_PORT'};
 
 my $heap              = "6G";
 my $threads = 16;
@@ -35,13 +31,9 @@ if (($auctions % 2) != 0) {
 	$auctions++;
 }
 
-my @mongoHostPortPairs = split /,/, $mongoHostPortPairsString;
-
 # Load the data
 my $dbLoaderOptions = "-d /items.json -t $threads ";
 $dbLoaderOptions .= " -u $maxUsers ";
-$dbLoaderOptions .= " -m $numNosqlShards ";
-$dbLoaderOptions .= " -p $numNosqlReplicas ";
 $dbLoaderOptions .= " -f $maxDuration ";
 $dbLoaderOptions .= " -a 'Workload $workloadNum, appInstance $appInstanceNum.' ";
 $dbLoaderOptions .= " -r \"/images\"";
@@ -52,6 +44,9 @@ my $dbLoaderJavaOptions = "";
 
 my $dbLoaderClasspath = "/dbLoader.jar:/dbLoaderLibs/*:/dbLoaderLibs";
 
-my $cmdString =
-"java -Xmx$heap -Xms$heap $dbLoaderJavaOptions -Dwkld=W${workloadNum}I${appInstanceNum} -cp $dbLoaderClasspath -Dspring.profiles.active=\"$springProfilesActive\" -DDBHOSTNAME=$dbHostname -DDBPORT=$dbPort -DMONGODB_HOST=$mongodbHostname -DMONGODB_PORT=$mongodbPort -DMONGODB_REPLICA_SET=$mongodbReplicaSet com.vmware.weathervane.auction.dbloader.DBLoader $dbLoaderOptions 2>/dev/null";
+my $cmdString = "java -Xmx$heap -Xms$heap $dbLoaderJavaOptions -Dwkld=W${workloadNum}I${appInstanceNum}" + 
+				" -cp $dbLoaderClasspath -Dspring.profiles.active=\"$springProfilesActive\"" + 
+				" -DDBHOSTNAME=$dbHostname -DDBPORT=$dbPort -DCASSANDRA_CONTACTPOINTS=$cassandraContactpoints" + 
+				" -DCASSANDRA_PORT=$cassandraPort com.vmware.weathervane.auction.dbloader.DBLoader $dbLoaderOptions 2>/dev/null";
+				
 system($cmdString);

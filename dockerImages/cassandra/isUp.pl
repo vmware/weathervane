9199122f8a69efc28ee70fcbc933015f457446d0
Hal Rosenberg <hrosenbe@vmware.com>
2019-05-23 12:33:44 -0400

    Fix isUp for cassandra
    Signed-off-by: Hal Rosenberg <hrosenbe@vmware.com>

--- dockerImages/cassandra/isUp.pl
+++ dockerImages/cassandra/isUp.pl
@@ -4,10 +4,22 @@ use strict;
 use POSIX;
 
 my $ip = `hostname -i`;
+chomp($ip);
 
-my $status = `nodetool status | grep $ip`;
-if ($status =~ /UN/) {
-	exit 0;
-} else {
-	exit 1;
+open my $cmd, '-|', 'nodetool status';
+while (my $line = <$cmd>) {
+	if ($line =~ /^UN\s+$ip/) {
+		sleep 10;
+		open my $cmd2, '-|', 'nodetool status';
+		while (my $line2 = <$cmd2>) {
+			if ($line2 =~ /^UN\s+$ip/) {
+				close $cmd;
+				close $cmd2;
+				exit 0;
+			}
+		}
+		close $cmd2;
+	}
 }
+close $cmd;
+exit 1;

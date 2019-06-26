449bcb5aa7a069e281356fc1c4e7fcad92bdb9e0
Hal Rosenberg <hrosenbe@vmware.com>
2019-06-26 12:06:42 -0400

    Enable jmx for driver and bidservice
    Signed-off-by: Hal Rosenberg <hrosenbe@vmware.com>

--- runHarness/Services/AuctionBidKubernetesService.pm
+++ runHarness/Services/AuctionBidKubernetesService.pm
@@ -65,6 +65,13 @@ sub configure {
 	if ( $self->getParamValue('logLevel') >= 3 ) {
 		$completeJVMOpts .= " -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -Xloggc:$tomcatCatalinaBase/logs/gc.log ";
 	}
+	if ( $self->getParamValue('auctionBidServerEnableJmx') ) {
+		$completeJVMOpts .= " -Dcom.sun.management.jmxremote.rmi.port=9090 -Dcom.sun.management.jmxremote=true "
+							. "-Dcom.sun.management.jmxremote.port=9090 -Dcom.sun.management.jmxremote.ssl=false "
+							. "-Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.local.only=false "
+							. "-Djava.rmi.server.hostname=127.0.0.1 ";
+	}
+	$completeJVMOpts .= " -DnodeNumber=$nodeNum ";
 	$completeJVMOpts .= " -DnodeNumber=$nodeNum ";
 	
 	my $numAuctionBidServers = $self->appInstance->getTotalNumOfServiceType('auctionBidServer');

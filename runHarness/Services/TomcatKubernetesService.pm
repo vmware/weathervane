538cfdcdaf85008f30d41375da4ae0e0a1856818
Hal Rosenberg <hrosenbe@vmware.com>
2019-06-26 10:45:13 -0400

    adjust jmx parameters
    Signed-off-by: Hal Rosenberg <hrosenbe@vmware.com>

--- runHarness/Services/TomcatKubernetesService.pm
+++ runHarness/Services/TomcatKubernetesService.pm
@@ -75,7 +75,7 @@ sub configure {
 		$completeJVMOpts .= " -Dcom.sun.management.jmxremote.rmi.port=9090 -Dcom.sun.management.jmxremote=true "
 							. "-Dcom.sun.management.jmxremote.port=9090 -Dcom.sun.management.jmxremote.ssl=false "
 							. "-Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.local.only=false "
-							. "-Djava.rmi.server.hostname=0.0.0.0 ";
+							. "-Djava.rmi.server.hostname=127.0.0.1 ";
 	}
 	$completeJVMOpts .= " -DnodeNumber=$nodeNum ";
 	

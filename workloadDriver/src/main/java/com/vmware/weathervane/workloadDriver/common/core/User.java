b572c481c4aa490af1e8fc7ae1afca8adbc96131
Hal Rosenberg <hrosenbe@vmware.com>
2019-07-09 16:53:29 -0400

    treat channel allocation as a user-level failure and don't retry
    Signed-off-by: Hal Rosenberg <hrosenbe@vmware.com>

--- workloadDriver/src/main/java/com/vmware/weathervane/workloadDriver/common/core/User.java
+++ workloadDriver/src/main/java/com/vmware/weathervane/workloadDriver/common/core/User.java
@@ -27,7 +27,6 @@ import java.util.UUID;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
-import java.util.concurrent.TimeUnit;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;

e2c2010b514eb6ca546550947c8dd15feeeff3ce
Hal Rosenberg <hrosenbe@vmware.com>
2019-08-02 15:50:37 -0400

    Capture errors from preTouch
    Signed-off-by: Hal Rosenberg <hrosenbe@vmware.com>

--- dockerImages/cassandra/pretouchData.sh
+++ dockerImages/cassandra/pretouchData.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 echo "Warming cassandra database"
-cqlsh -f /pretouchData.cql --request-timeout=3600 $HOSTNAME > /dev/null
+cqlsh -f /pretouchData.cql --request-timeout=3600 $HOSTNAME > /dev/null 2> /pretouchError.log
+cat /pretouchError.log

7fff041fbd6d2ea4d96b6428519ccad46822229f
Hal Rosenberg <hrosenbe@vmware.com>
2019-08-13 15:47:02 -0400

    clean up postgresql index names
    Signed-off-by: Hal Rosenberg <hrosenbe@vmware.com>

--- dockerImages/postgresql/pg-init.sh
+++ dockerImages/postgresql/pg-init.sh
@@ -60,6 +60,9 @@ echo "Reindex"
 sudo -u postgres /usr/pgsql-${PG_MAJOR}/bin/psql -p ${POSTGRESPORT} -U auction -d auction -c "reindex database auction;"
 
 # Force a vacuum and checkpoint
+echo "Force vacuum full"
+sudo -u postgres /usr/pgsql-${PG_MAJOR}/bin/psql -p ${POSTGRESPORT} -U auction -d auction -c "vacuum full;"
+# Force a vacuum and checkpoint
 echo "Force vacuum"
 sudo -u postgres /usr/pgsql-${PG_MAJOR}/bin/psql -p ${POSTGRESPORT} -U auction -d auction -c "vacuum analyze;"
 echo "Force checkpoint"

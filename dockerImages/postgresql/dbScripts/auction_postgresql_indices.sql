12dcfb72daeaf2c94e034f257f6c8b04c5c23586
Hal Rosenberg <hrosenbe@vmware.com>
2019-08-13 15:47:02 -0400

    clean up postgresql index names
    Signed-off-by: Hal Rosenberg <hrosenbe@vmware.com>

--- dockerImages/postgresql/dbScripts/auction_postgresql_indices.sql
+++ dockerImages/postgresql/dbScripts/auction_postgresql_indices.sql
@@ -15,26 +15,26 @@ SET default_tablespace = '';
 
 SET default_with_oids = false;
 
-CREATE INDEX auction_auctioneer_id_key ON auction USING btree (auctioneer_id);
+CREATE INDEX auction_auctioneer_id_idx ON auction USING btree (auctioneer_id);
 CREATE INDEX auction_starttime_idx ON auction USING btree (starttime);
 CREATE INDEX auction_state_idx ON auction USING btree (state);
 CREATE INDEX auction_current_activated_idx ON auction USING btree (current, activated);
 
-CREATE INDEX auction_keyword_auction_key ON auction_keyword USING btree (auction_id);
-CREATE INDEX auction_keyword_keyword_key ON auction_keyword USING btree (keyword_id);
+CREATE INDEX auction_keyword_auction_idx ON auction_keyword USING btree (auction_id);
+CREATE INDEX auction_keyword_keyword_idx ON auction_keyword USING btree (keyword_id);
 
-CREATE INDEX highbid_bidder_id_key ON highbid USING btree (bidder_id);
-CREATE INDEX highbid_item_id_key ON highbid USING btree (item_id);
-CREATE INDEX highbid_auction_id_key ON highbid USING btree (auction_id);
-CREATE INDEX highbid_state_key ON highbid USING btree (state, id);
-CREATE INDEX highbid_state_bidder_endtime_key ON highbid USING btree (state, bidder_id, biddingendtime);
+CREATE INDEX highbid_bidder_id_idx ON highbid USING btree (bidder_id);
+CREATE INDEX highbid_item_id_idx ON highbid USING btree (item_id);
+CREATE INDEX highbid_auction_id_idx ON highbid USING btree (auction_id);
+CREATE INDEX highbid_state_idx ON highbid USING btree (state, id);
+CREATE INDEX highbid_state_bidder_endtime_idx ON highbid USING btree (state, bidder_id, biddingendtime);
 
-CREATE INDEX bidcompletiondelay_bid_id_key ON bidcompletiondelay USING btree (bidid);
-CREATE INDEX bidcompletiondelay_item_id_key ON bidcompletiondelay USING btree (item_id);
+CREATE INDEX bidcompletiondelay_bid_id_idx ON bidcompletiondelay USING btree (bidid);
+CREATE INDEX bidcompletiondelay_item_id_idx ON bidcompletiondelay USING btree (item_id);
 
-CREATE INDEX item_auction_id_key ON item USING btree (auction_id);
-CREATE INDEX item_auction_id_id_key ON item USING btree (auction_id,id);
-CREATE INDEX item_auctioneer_id_key ON item USING btree (auctioneer_id);
+CREATE INDEX item_auction_id_idx ON item USING btree (auction_id);
+CREATE INDEX item_auction_id_id_idx ON item USING btree (auction_id,id);
+CREATE INDEX item_auctioneer_id_idx ON item USING btree (auctioneer_id);
 CREATE INDEX item_preloaded_idx ON item USING btree (preloaded);
 
 CREATE INDEX user_authtoken_idx ON userdata USING btree (authtoken);

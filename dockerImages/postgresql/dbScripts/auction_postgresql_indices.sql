bca1f411266b726ca9f492b28e9cf9a2e29232b8
Hal Rosenberg <hrosenbe@vmware.com>
2019-10-16 15:20:44 -0400

    drop and recreate indices to avoid concurrent delete issue
    Signed-off-by: Hal Rosenberg <hrosenbe@vmware.com>

--- dockerImages/postgresql/dbScripts/auction_postgresql_indices.sql
+++ dockerImages/postgresql/dbScripts/auction_postgresql_indices.sql
@@ -15,36 +15,64 @@ SET default_tablespace = '';
 
 SET default_with_oids = false;
 
-CREATE INDEX auction_auctioneer_id_idx ON auction USING btree (auctioneer_id);
-CREATE INDEX auction_starttime_idx ON auction USING btree (starttime);
-CREATE INDEX auction_state_idx ON auction USING btree (state);
-CREATE INDEX auction_current_activated_idx ON auction USING btree (current, activated);
+DROP INDEX auction_auctioneer_id_idx;
+DROP INDEX auction_starttime_idx;
+DROP INDEX auction_state_idx;
+DROP INDEX auction_current_activated_idx;
 
-CREATE INDEX auction_keyword_auction_idx ON auction_keyword USING btree (auction_id);
-CREATE INDEX auction_keyword_keyword_idx ON auction_keyword USING btree (keyword_id);
+DROP INDEX highbid_preloaded_idx;
+DROP INDEX highbid_bidder_id_idx;
+DROP INDEX highbid_item_id_idx;
+DROP INDEX highbid_auction_id_idx;
+DROP INDEX highbid_state_idx;
+DROP INDEX highbid_state_bidder_endtime_idx;
 
-CREATE INDEX highbid_preloaded_idx ON highbid USING btree (preloaded);
-CREATE INDEX highbid_bidder_id_idx ON highbid USING btree (bidder_id);
-CREATE INDEX highbid_item_id_idx ON highbid USING btree (item_id);
-CREATE INDEX highbid_auction_id_idx ON highbid USING btree (auction_id);
-CREATE INDEX highbid_state_idx ON highbid USING btree (state, id);
-CREATE INDEX highbid_state_bidder_endtime_idx ON highbid USING btree (state, bidder_id, biddingendtime);
+DROP INDEX bidcompletiondelay_bid_id_idx;
+DROP INDEX bidcompletiondelay_item_id_idx;
+DROP INDEX bidcompletiondelay_auction_id_idx;
 
-CREATE INDEX bidcompletiondelay_bid_id_idx ON bidcompletiondelay USING btree (bidid);
-CREATE INDEX bidcompletiondelay_item_id_idx ON bidcompletiondelay USING btree (item_id);
-CREATE INDEX bidcompletiondelay_auction_id_idx ON bidcompletiondelay USING btree (auction_id);
+DROP INDEX item_auction_id_idx;
+DROP INDEX item_highbid_id_idx;
+DROP INDEX item_auctioneer_id_idx;
+DROP INDEX item_auction_id_id_idx;
+DROP INDEX item_preloaded_idx;
 
-CREATE INDEX item_auction_id_idx ON item USING btree (auction_id);
-CREATE INDEX item_highbid_id_idx ON item USING btree (highbid_id);
-CREATE INDEX item_auctioneer_id_idx ON item USING btree (auctioneer_id);
-CREATE INDEX item_auction_id_id_idx ON item USING btree (auction_id,id);
-CREATE INDEX item_preloaded_idx ON item USING btree (preloaded);
+DROP INDEX user_authtoken_idx;
+DROP INDEX user_email_idx;
 
-CREATE INDEX user_authtoken_idx ON userdata USING btree (authtoken);
-CREATE INDEX user_email_idx ON userdata USING btree (email);
+DROP INDEX keyword_keyword_idx;
 
-CREATE INDEX keyword_keyword_idx ON keyword USING btree (keyword);
+DROP INDEX auction_keyword_keyword_idx;
+DROP INDEX auction_keyword_auction_idx;
 
-CREATE INDEX auction_keyword_keyword_idx ON auction_keyword USING btree (keyword_id);
-CREATE INDEX auction_keyword_auction_idx ON auction_keyword USING btree (auction_id);
+---
+CREATE INDEX CONCURRENTLY auction_auctioneer_id_idx ON auction USING btree (auctioneer_id);
+CREATE INDEX CONCURRENTLY auction_starttime_idx ON auction USING btree (starttime);
+CREATE INDEX CONCURRENTLY auction_state_idx ON auction USING btree (state);
+CREATE INDEX CONCURRENTLY auction_current_activated_idx ON auction USING btree (current, activated);
+
+CREATE INDEX CONCURRENTLY highbid_preloaded_idx ON highbid USING btree (preloaded);
+CREATE INDEX CONCURRENTLY highbid_bidder_id_idx ON highbid USING btree (bidder_id);
+CREATE INDEX CONCURRENTLY highbid_item_id_idx ON highbid USING btree (item_id);
+CREATE INDEX CONCURRENTLY highbid_auction_id_idx ON highbid USING btree (auction_id);
+CREATE INDEX CONCURRENTLY highbid_state_idx ON highbid USING btree (state, id);
+CREATE INDEX CONCURRENTLY highbid_state_bidder_endtime_idx ON highbid USING btree (state, bidder_id, biddingendtime);
+
+CREATE INDEX CONCURRENTLY bidcompletiondelay_bid_id_idx ON bidcompletiondelay USING btree (bidid);
+CREATE INDEX CONCURRENTLY bidcompletiondelay_item_id_idx ON bidcompletiondelay USING btree (item_id);
+CREATE INDEX CONCURRENTLY bidcompletiondelay_auction_id_idx ON bidcompletiondelay USING btree (auction_id);
+
+CREATE INDEX CONCURRENTLY item_auction_id_idx ON item USING btree (auction_id);
+CREATE INDEX CONCURRENTLY item_highbid_id_idx ON item USING btree (highbid_id);
+CREATE INDEX CONCURRENTLY item_auctioneer_id_idx ON item USING btree (auctioneer_id);
+CREATE INDEX CONCURRENTLY item_auction_id_id_idx ON item USING btree (auction_id,id);
+CREATE INDEX CONCURRENTLY item_preloaded_idx ON item USING btree (preloaded);
+
+CREATE INDEX CONCURRENTLY user_authtoken_idx ON userdata USING btree (authtoken);
+CREATE INDEX CONCURRENTLY user_email_idx ON userdata USING btree (email);
+
+CREATE INDEX CONCURRENTLY keyword_keyword_idx ON keyword USING btree (keyword);
+
+CREATE INDEX CONCURRENTLY auction_keyword_keyword_idx ON auction_keyword USING btree (keyword_id);
+CREATE INDEX CONCURRENTLY auction_keyword_auction_idx ON auction_keyword USING btree (auction_id);
 
--- dockerImages/postgresql/pg-init.sh
+++ dockerImages/postgresql/pg-init.sh
@@ -61,9 +61,9 @@ while /usr/pgsql-9.3/bin/pg_isready -h 127.0.0.1 -p 5432 ; [ $? -ne 0 ]; do
     echo "Waiting for PostgreSQL to be ready"
 done
 
-# Reindex
-echo "Reindex"
-sudo -u postgres /usr/pgsql-${PG_MAJOR}/bin/psql -p ${POSTGRESPORT} -U auction -d auction -c "reindex database auction;"
+# Recreate indices
+echo "Recreate indices"
+sudo -u postgres /usr/pgsql-${PG_MAJOR}/bin/psql -p ${POSTGRESPORT} -U auction -d auction -f /dbScripts/auction_postgresql_indices.sql
 
 # Force a vacuum and checkpoint
 echo "Force vacuum full"

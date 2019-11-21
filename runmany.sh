0ff4c92ad44b6431763b5f8276976a35d1f52de9
Hal Rosenberg <hrosenbe@vmware.com>
2019-11-21 10:00:29 -0500

    update runmany.sh
    Signed-off-by: Hal Rosenberg <hrosenbe@vmware.com>

--- runmany.sh
+++ runmany.sh
@@ -1,22 +1,26 @@
 #!/bin/sh
 
-# You can put multiple calls of the weathervane.pl script in this file, and 
+# You can put multiple calls of the runWeathervane.pl script in this file, and 
 # then run this file to start a series of runs.  You can either use a separate
 # configuration file for each run, or use command-line options to override 
-# options set in the config file.  Below are some examples of
+# options set in the configuration file.  Below are some examples of
 # using the script.  You can uncomment them and edit as appropriate,
 
-# This is a basic invocation that only uses options set in the 
-# default weathervane.config file
-#./weathervane.pl
+# This is a basic invocation that only uses options set in the specified 
+# configuration file. To use this you would replace weathervane.config
+# with the name of your configuration file.
+#./runWeathervane.pl --configFile weathervane.config
 
-# These invocations overrids the number of users set in the config file.
-# Notice that the output of the run script is redirected to a file named console.log.  
-# This is done so that it can be checked for errors after the runs are complete. The
-# progress of the run can be followed with `tail -f console.log`
-#./weathervane.pl --users=500 
-#./weathervane.pl --users=1000  
+# These invocations override the runStrategy and number of users set in the 
+# configuration file using command-line parameters. Notice that the --
+# is required between the parameters to the runWeathervane.pl script and
+# the command-line parameters.
+#./runWeathervane.pl --configFile weathervane.config -- --runStrategy=fixed --users=500 
+#./runWeathervane.pl --configFile weathervane.config -- --runStrategy=fixed --users=1000 
 
-# The following invocation can be used to run the workload in maximum finding mode.
-./weathervane.pl --users=200 --runStrategy=findMax --initialRateStep=100 --minRateStep=50 --runLength=medium --description="initial test"  
+# The following invocation can be used to run the findMaxSingleRun runStrategy, 
+# even if the configuration file specifies the fixed runStrategy.
+#./runWeathervane.pl --configFile weathervane.config -- --runStrategy=fixed
 
+# You can add as many additional invocations as desired in this file.  They will 
+# run sequentially.

7f63ba977a6a4ace7781f54e0bdf78e14b5be0fe
Hal Rosenberg <hrosenbe@vmware.com>
2019-12-02 14:18:27 -0500

    Remove pre-generated certificates
    Signed-off-by: Hal Rosenberg <hrosenbe@vmware.com>

---
+++ dockerImages/auctionbidservice/generateCert.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+subject="/C=US/ST=MI/L=AnnArbor/O=weathervane/OU=auction/CN=www.weathervane"
+
+# Generate a key for signing the certificate
+openssl genrsa -out /etc/pki/tls/private/weathervane.key 2048 
+
+# Create a CSR
+openssl req -new -key /etc/pki/tls/private/weathervane.key \
+    -out /etc/pki/tls/certs/weathervane.csr -subj $subject
+
+# Create the certificate
+openssl x509 -req -days 730 -in /etc/pki/tls/certs/weathervane.csr \
+     -signkey /etc/pki/tls/private/weathervane.key -out /etc/pki/tls/certs/weathervane.crt
+
+# Generate a PKCS12 keystore
+openssl pkcs12 -export -in /etc/pki/tls/certs/weathervane.crt \
+    -inkey /etc/pki/tls/private/weathervane.key  -out /etc/pki/tls/weathervane.p12 \
+    -name weathervane -CAfile /etc/pki/tls/certs/weathervane.crt \
+    -caname weathervane -passout pass:weathervane
---
+++ dockerImages/nginx/generateCert.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+subject="/C=US/ST=MI/L=AnnArbor/O=weathervane/OU=auction/CN=www.weathervane"
+
+# Generate a key for signing the certificate
+openssl genrsa -out /etc/pki/tls/private/weathervane.key 2048 
+
+# Create a CSR
+openssl req -new -key /etc/pki/tls/private/weathervane.key -out /etc/pki/tls/certs/weathervane.csr -subj $subject
+
+# Create the certificate
+openssl x509 -req -days 730 -in /etc/pki/tls/certs/weathervane.csr -signkey /etc/pki/tls/private/weathervane.key -out /etc/pki/tls/certs/weathervane.crt

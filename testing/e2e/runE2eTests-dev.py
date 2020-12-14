e07bc4bc6a83e658cd4057a6685f5abaccb14c09
Rebecca Yo <griderr@vmware.com>
2020-12-14 15:48:46 -0600

    Update runE2eTests-dev to use Weathervane config files from repository
    Signed-off-by: Rebecca Yo <griderr@vmware.com>

--- testing/e2e/runE2eTests-dev.py
+++ testing/e2e/runE2eTests-dev.py
@@ -4,10 +4,14 @@ import sys
 
 return_code = 0
 
+# runWeathervane.pl configFile argument requires an absolute file path to the config file.
+# Get current working directory to interpolate the absolute file path.
+cwd = os.getcwd()
+
 return_code = os.system("touch .accept-weathervane")
-return_code += os.system("./runWeathervane.pl --configFile=/root/weathervane/testing/e2e/weathervaneConfigFiles/weathervane.config.k8s.micro-dev")
-return_code += os.system("./runWeathervane.pl --configFile=/root/weathervane/testing/e2e/weathervaneConfigFiles/weathervane.config.k8s.xsmall-dev")
-return_code += os.system("./runWeathervane.pl --configFile=/root/weathervane/testing/e2e/weathervaneConfigFiles/weathervane.config.k8s.small2-dev")
+return_code += os.system("./runWeathervane.pl --configFile={}/testing/e2e/weathervaneConfigFiles/weathervane.config.k8s.micro-dev".format(cwd))
+return_code += os.system("./runWeathervane.pl --configFile={}/testing/e2e/weathervaneConfigFiles/weathervane.config.k8s.xsmall-dev".format(cwd))
+return_code += os.system("./runWeathervane.pl --configFile={}/testing/e2e/weathervaneConfigFiles/weathervane.config.k8s.small2-dev".format(cwd))
 
 #print("The return code was: %d" % return_code)
 if return_code > 0:

244b5ea5423e448b7806405da6223a1ab57f24d7
Hal Rosenberg <hrosenbe@vmware.com>
2021-01-14 15:42:45 -0500

    parallelize notification of sync-ed loadPaths
    Signed-off-by: Hal Rosenberg <hrosenbe@vmware.com>

--- workloadDriver/src/main/java/com/vmware/weathervane/workloadDriver/common/core/loadControl/loadPathController/BaseLoadPathController.java
+++ workloadDriver/src/main/java/com/vmware/weathervane/workloadDriver/common/core/loadControl/loadPathController/BaseLoadPathController.java
@@ -4,20 +4,35 @@ SPDX-License-Identifier: BSD-2-Clause
 */
 package com.vmware.weathervane.workloadDriver.common.core.loadControl.loadPathController;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.vmware.weathervane.workloadDriver.common.core.Workload;
+import com.vmware.weathervane.workloadDriver.common.core.loadControl.loadInterval.RampLoadInterval;
+
 public abstract class BaseLoadPathController implements LoadPathController {
+
 	private static final Logger logger = LoggerFactory.getLogger(BaseLoadPathController.class);
 
 	protected Map<String, LoadPathIntervalResultWatcher> watchers = new HashMap<>();
 	protected int numWatchers = 0;
 	protected Map<String, Integer> numIntervalResults = new HashMap<>();
 	protected Map<String, Boolean> intervalResults = new HashMap<>();
+
+	private ScheduledExecutorService executorService;
+
+	public void initialize(ScheduledExecutorService executorService) {
+		this.executorService = executorService;
+	}
 	
 	@Override
 	public void registerIntervalResultCallback(String name, LoadPathIntervalResultWatcher watcher) {
@@ -59,9 +74,41 @@ public abstract class BaseLoadPathController implements LoadPathController {
 									boolean latestResult, boolean isLastInInterval);
 	
 	protected void notifyWatchers(String intervalName, boolean passed) {
+		logger.debug("notifyWatchers for interval {}, result = {}", intervalName, passed);
+		/*
+		 * Notify the watchers in parallel to avoid waiting for all of the driver nodes 
+		 * to be notified of changes in the number of users
+		 */
+		List<ScheduledFuture<?>> sfList = new ArrayList<>();
 		for (Entry<String, LoadPathIntervalResultWatcher> entry : watchers.entrySet()) {
-			entry.getValue().intervalResult(intervalName, passed);
+			logger.debug("notifyWatchers for interval {}, scheduling a notification", intervalName);
+			ScheduledFuture<?> sf = executorService.schedule(new Runnable() {
+				
+				@Override
+				public void run() {
+					logger.debug("notifyWatchers for interval {}, in scheduled notification");
+					entry.getValue().intervalResult(intervalName, passed);					
+				}
+			}, 0, TimeUnit.SECONDS);
+			logger.debug("notifyWatchers for interval {}, scheduled a notification", intervalName);
+			sfList.add(sf);
 		}
+		logger.debug("notifyWatchers for interval {}, scheduled all notifications", intervalName, passed);
+		
+		/*
+		 * Now wait for all of the watchers to be notified
+		 */
+		sfList.stream().forEach(sf -> {
+			try {
+				logger.debug("notifyWatchers for interval {}, getting a result of a notification", intervalName, passed);
+				sf.get(); 
+			} catch (Exception e) {
+				logger.warn("When notifying watcher for interval " + intervalName 
+						+ " get exception: " + e.getMessage());
+			};
+		});
 	}
+	
+	
 	
 }

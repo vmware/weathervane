6f9c8149d80b3211e211a06a7502658cb58b9ae5
Luis Alvarez Sanchez <lalvarezsanc@vmware.com>
2022-02-10 08:57:54 -0800

    Removing unnecessary backslash Signed-off-by: Luis Alvarez Sanchez <lalvarezsanc@vmware.com>

--- buildDockerImages.pl
+++ buildDockerImages.pl
@@ -196,8 +196,8 @@ sub setupForBuild {
 sub cleanupAfterBuild {
 	my ($fileout) = @_;
 	#cleaning extraneous files from previous runs
-	runAndLog($fileout, "docker images -a | grep \"weathervane*\|openjdk*\|centos*\" | awk '{print \$3}' | xargs docker rmi"); # removing images
-	runAndLog($fileout, "docker rm \$(docker ps -a | grep \"weathervane*\" | awk '{print \$1}')"); # Removing containers
+	runAndLog($fileout, "docker images -a | grep \"weathervane*\|openjdk*\|centos*\" | awk '{print $3}' | xargs docker rmi"); # removing images
+	runAndLog($fileout, "docker rm \$(docker ps -a | grep \"weathervane*\" | awk '{print $1}')"); # Removing containers
 	
 	runAndLog($fileout, "rm -rf ./dockerImages/nginx/html");
 	runAndLog($fileout, "rm -f ./dockerImages/auctionappserverwarmer/auctionAppServerWarmer.jar");

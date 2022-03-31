0da134757c6e3a24065bcf89987c2d8077ec8646
Rebecca Yo <griderr@vmware.com>
2022-03-31 17:38:00 -0500

    Add small3 to CICD
    Signed-off-by: Rebecca Yo <griderr@vmware.com>

--- testing/e2e/runE2eTests.py
+++ testing/e2e/runE2eTests.py
@@ -1,4 +1,3 @@
-
 import os
 import sys
 
@@ -8,11 +7,22 @@ return_code = 0
 # Get current working directory to interpolate the absolute file path.
 cwd = os.getcwd()
 
+# On the first run of Weathervane, the user is asked to accept Weathervane terms. On completion,
+# the file .accept-weathervane is created. This script creates the file which is required for 
+# weathervane to run.
 return_code = os.system("touch .accept-weathervane")
+
+# Start one findmax run for each configuration size. Small2 is deprecated but still supported 
+# as of Weathervane 2.1.1.
+# redeploy flag is required to make sure all new containers are pulled each time as code may have 
+# changed since the previous CICD execution.
 return_code += os.system("./runWeathervane.pl --configFile={}/testing/e2e/weathervaneConfigFiles/weathervane.config.k8s.micro -- --redeploy".format(cwd))
 return_code += os.system("./runWeathervane.pl --configFile={}/testing/e2e/weathervaneConfigFiles/weathervane.config.k8s.xsmall -- --redeploy".format(cwd))
 return_code += os.system("./runWeathervane.pl --configFile={}/testing/e2e/weathervaneConfigFiles/weathervane.config.k8s.small2 -- --redeploy".format(cwd))
+return_code += os.system("./runWeathervane.pl --configFile={}/testing/e2e/weathervaneConfigFiles/weathervane.config.k8s.small3 -- --redeploy".format(cwd))
 
+# Return code processing allows the Jenkins CICD pipeline to detect testing pass/fail. If any
+# test fails, the entire stage fails.
 if return_code > 0:
 	sys.exit(1)
 else:
